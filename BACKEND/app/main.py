c0eb7f20068045f65125bd1796ccf78e46124ea7
JEELDONGA18 <jeeldonga18@gmail.com>
2026-06-07 01:06:03 +0530

    origin changed in backend..

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Smart Doc AI")
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["http://localhost:3000", "https://smart-doc-ai-new.vercel.app"],
-    allow_origins=["*"],
+    allow_origins=["https://smart-doc-ai-new.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
