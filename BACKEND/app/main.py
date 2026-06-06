6d7e61a422c84ff7b60dd0fa89008f26282580be
JEELDONGA18 <jeeldonga18@gmail.com>
2026-06-07 00:53:17 +0530

    All methods in the cors is added in backend..

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "https://smart-doc-ai-new.vercel.app"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
