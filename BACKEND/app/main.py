4ed73f9a19ff9210591f18d21cb2cdad2bcd66ce
JEELDONGA18 <jeeldonga18@gmail.com>
2026-06-06 22:08:24 +0530

    Frontend and Backend are ready to live..

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Smart Doc AI")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://smart-doc-ai-new.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
